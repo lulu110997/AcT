0b626ac99f865d5005e87cbbc98e0c8375fb2ac2
lulu110997 <louis97fernandez@gmail.com>
2024-01-31 18:09:10 +1100

    plotting losses between keras and pt

---
+++ keras_losses.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import os
+import pickle_wrapper as _pw
+k = "/home/louis/Data/Fernandez_HAR/AcT_keras_results/micro_trained_with_losses/losses/"
+pt = "/home/louis/Data/Fernandez_HAR/AcT_pt/micro_2024_01_31_15_09_44/plots"
+for split in range(1, 2):
+    for fold in range(2):
+        k_training_loss = f"training_loss_s{split}_f{fold}_micro.pickle"
+        k_validation_loss = f"validation_loss_s{split}_f{fold}_micro.pickle"
+        k_tl = _pw.open_pickle(os.path.join(k, k_training_loss))
+        k_vl = _pw.open_pickle(os.path.join(k, k_validation_loss))
+        pt_training_loss = f"s_{split}_f_{fold}_train_loss.pickle"
+        pt_validation_loss = f"s_{split}_f_{fold}_test_loss.pickle"
+        pt_tl = _pw.open_pickle(os.path.join(pt, pt_training_loss))
+        pt_vl = _pw.open_pickle(os.path.join(pt, pt_validation_loss))
+        fig, ax = plt.subplots()
+        ax.set_title(f'Loss for split {split} and fold {fold}')
+        ax.plot(k_tl, label='keras training loss')
+        ax.plot(k_vl, label='keras validation_loss')
+        ax.plot(pt_tl, label='pt training loss')
+        ax.plot(pt_vl, label='pt validation_loss')
+        plt.legend()
+        plt.show()
